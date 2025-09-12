df7602470fcfbbe9b43174a263472bfae6b73004
alenapigeeva <a.pigeeva@gmail.com>
2025-09-12 16:57:18 +0300

    Find info about nonexistent pet

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,8 +1,11 @@
+from http.client import responses
+
 import allure
 import requests
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3'
 idNonexistentPet = 9999
+dataNoneexistentPet = {"id": 9999, "name": "Non-existent Pet", "status": "available"}
 
 
 @allure.feature("Pet")
@@ -18,3 +21,28 @@ class TestPet:
 
         with allure.step('Проверка текста в ответе'):
             assert response.text == 'Pet deleted', 'Пришел другой текст в ответе'
+
+
+    @allure.title('Попытка обновить несуществующего питомца')
+    @allure.description('Данный тест берет данные несуществующего питомца и попытается их обновить')
+    def test_update_nonexistent_pet(self):
+
+        with allure.step('Отправка запроса на обновление несуществующего питомца'):
+            response = requests.put(f'{BASE_URL}/pet', dataNoneexistentPet)
+
+        with allure.step('Проверка статус кода'):
+                assert response.status_code == 404, 'Статус кода не соответствует документации'
+
+        with allure.step('Проверка текста ошибочного сообщения'):
+                assert response.text == 'Pet not found', 'Пришел другой текст'
+
+    @allure.title('Попытка получить информацию о несуществующем питомце')
+    def test_get_info_about_nonexistent_pet(self):
+        with allure.step('Отправляем запрос на получение информации'):
+            response = requests.get(f'{BASE_URL}/pet/{idNonexistentPet}')
+
+        with allure.step('Проверка статус кода'):
+            assert response.status_code == 404, 'Статус кода не соответствует документации'
+
+        with allure.step('Проверка текста в ответе'):
+            assert response.text == 'Pet not found', 'Пришел другой текст'
