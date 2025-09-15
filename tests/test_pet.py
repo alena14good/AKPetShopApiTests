629a58e93c3153a12b7e6d3c6f39ee73b0d613ac
alenapigeeva <a.pigeeva@gmail.com>
2025-09-15 13:11:37 +0300

    Renamed the variables and placed the variable data_noneexistent_pet inside the function

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -4,17 +4,17 @@ import allure
 import requests
 
 BASE_URL = 'http://5.181.109.28:9090/api/v3'
-idNonexistentPet = 9999
-dataNoneexistentPet = {"id": 9999, "name": "Non-existent Pet", "status": "available"}
+id_nonexistent_Pet = 9999
 
 
 @allure.feature("Pet")
 class TestPet:
     @allure.title('Попытка удалить несуществующего питомца')
     @allure.description('Тест описания Allure. Сейчас будет проводиться запрос на удаление несуществующего питомца.')
-    def test_delete_nonexistent_pet(self):
+    def \
+            test_delete_nonexistent_pet(self):
         with allure.step('Удаляем несуществующего питомца'):
-            response = requests.delete(f'{BASE_URL}/pet/{idNonexistentPet}')
+            response = requests.delete(f'{BASE_URL}/pet/{id_nonexistent_Pet}')
 
         with allure.step('Проверка статус кода'):
             assert response.status_code == 200, 'Статус код пришел не по ТЗ (не 200)'
@@ -28,7 +28,8 @@ class TestPet:
     def test_update_nonexistent_pet(self):
 
         with allure.step('Отправка запроса на обновление несуществующего питомца'):
-            response = requests.put(f'{BASE_URL}/pet', dataNoneexistentPet)
+            data_noneexistent_pet = {"id": 9999, "name": "Non-existent Pet", "status": "available"}
+            response = requests.put(f'{BASE_URL}/pet',  data_noneexistent_pet)
 
         with allure.step('Проверка статус кода'):
                 assert response.status_code == 404, 'Статус кода не соответствует документации'
@@ -39,7 +40,7 @@ class TestPet:
     @allure.title('Попытка получить информацию о несуществующем питомце')
     def test_get_info_about_nonexistent_pet(self):
         with allure.step('Отправляем запрос на получение информации'):
-            response = requests.get(f'{BASE_URL}/pet/{idNonexistentPet}')
+            response = requests.get(f'{BASE_URL}/pet/{id_nonexistent_Pet}')
 
         with allure.step('Проверка статус кода'):
             assert response.status_code == 404, 'Статус кода не соответствует документации'
