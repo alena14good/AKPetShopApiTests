a33bcba1e84368f5bc7a5721da3a6cfa543d89dd
alenapigeeva <a.pigeeva@gmail.com>
2025-09-21 22:15:27 +0300

    Added parametrize test

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,6 +1,7 @@
 from http.client import responses
 
 import jsonschema
+import pytest
 
 from .schemas.pet_schema import PET_SCHEMA
 import allure
@@ -70,7 +71,7 @@ class TestPet:
 
         with allure.step('Отправка POST-запроса с тестовыми данными'):
             response = requests.post(f'{BASE_URL}/pet', json=payload)
-            response_json = response.json() # вывели результат функции в переменную, чтобы в последних ассертах вызывать переменную, а не функцию
+            response_json = response.json()  # вывели результат функции в переменную, чтобы в последних ассертах вызывать переменную, а не функцию
 
         with allure.step('Проверка статус кода'):
             assert response.status_code == 200, 'Запрос прошел с другим статус кодом'
@@ -81,14 +82,18 @@ class TestPet:
         with allure.step('Проверка, что создалась запись с корректными данными'):
             assert response_json['id'] == payload['id'], f'Создалась запись не с {payload['id']}'
             assert response_json['name'] == payload['name'], f'Создалась запись не с {payload['name']}'
-            assert response_json['category']['id'] == payload['category']['id'], f'Создалась запись не с {payload['category']['id']}'
-            assert response_json['category']['name'] == payload['category']['name'], f'Создалась запись не с {payload['category']['name']}'
+            assert response_json['category']['id'] == payload['category'][
+                'id'], f'Создалась запись не с {payload['category']['id']}'
+            assert response_json['category']['name'] == payload['category'][
+                'name'], f'Создалась запись не с {payload['category']['name']}'
             assert response_json['photoUrls'] == payload['photoUrls'], f'Создалась запись не с {payload['photoUrls']}'
-            assert response_json['tags'][0]['id']  == payload['tags'][0]['id'] , f'Создалась запись не с {payload['tags'][0]['id'] }'
-            assert response_json['tags'][0]['name'] == payload['tags'][0]['name'], f'Создалась запись не с {payload['tags'][0]['name']}'
+            assert response_json['tags'][0]['id'] == payload['tags'][0][
+                'id'], f'Создалась запись не с {payload['tags'][0]['id']}'
+            assert response_json['tags'][0]['name'] == payload['tags'][0][
+                'name'], f'Создалась запись не с {payload['tags'][0]['name']}'
             assert response_json['status'] == payload['status'], f'Создалась запись не с {payload['status']}'
 
-    @allure.title ("Обновление информации о питомце (PUT /pet)")
+    @allure.title("Обновление информации о питомце (PUT /pet)")
     def test_update_info_about_pet(self, create_pet):
         with allure.step('Подготовка тестовых данных'):
             pet_id = create_pet["id"]
@@ -119,3 +124,21 @@ class TestPet:
         with allure.step('Проверка, что питомец удален и его нет в бд'):
             response = requests.get(f'{BASE_URL}/pet/{pet_id}')
             assert response.status_code == 404, "Другой статус код"
+
+    @allure.title('Получение списка питомцев по статусу (GET /pet/findByStatus)')
+    @pytest.mark.parametrize(
+        "status, expected_status_code",
+        [
+            ("available", 200),
+            ("pending", 200),
+            ("sold", 200),
+            # ("nonexistance status", 400) - оставить это  вариант пока не получается, иначе падает последний assert, необходимо больше знаний, как я поняла if/else помогло бы тут
+        ]
+    )
+    def test_get_pets_by_status(self, status, expected_status_code):
+        with allure.step(f'Отправка GET-запроса со статусом {status}'):
+            response = requests.get(f'{BASE_URL}/pet/findByStatus', params={"status": status})
+
+        with allure.step('Проверка статус кода'):
+            assert response.status_code == expected_status_code, 'Статус код другой'
+            assert isinstance(response.json(), list), "Ответ не является списком"
